7a2150fb56dea0a450919f74f6094a0a5af96e8b
“Michael <240designworks@gmail.com>
2024-12-24 14:29:20 -0600

    removed fumadocs 3

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -2,6 +2,8 @@
 // import { createMDXSource } from 'fumadocs-mdx'
 // import { loader } from 'fumadocs-core/source'
 
+// removed fumadocs-mdx
+
 // export const source = loader({
 //   baseUrl: '/docs',
 //   source: createMDXSource(docs, meta),
