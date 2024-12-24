7d61d0caf28a226e3a71e6149e2e21ae237640bd
“Michael <240designworks@gmail.com>
2024-12-23 22:22:09 -0600

    updated signUp to allow build

--- src/app/(app)/sign-up/actions/formSubmit.ts
+++ src/app/(app)/sign-up/actions/formSubmit.ts
@@ -1,56 +1,66 @@
-'use server'
-import { schema } from '../schema'
-import { SignUp } from './signUp'
+"use server"
+import { schema } from "../schema"
+// import { SignUp } from './signUp'
 
 export type FormState = {
-  message: string
-  fields?: Record<string, string>
-  status: 'success' | 'error' | ''
-  issues?: string[]
+	message: string
+	fields?: Record<string, string>
+	status: "success" | "error" | ""
+	issues?: string[]
 }
 
-export async function onSubmitAction(prevState: FormState, data: FormData): Promise<FormState> {
-  const formData = Object.fromEntries(data)
-  const parsed = schema.safeParse(formData)
+export async function onSubmitAction(
+	prevState: FormState,
+	data: FormData
+): Promise<FormState> {
+	const formData = Object.fromEntries(data)
+	const parsed = schema.safeParse(formData)
 
-  console.log('Parsed', parsed)
+	console.log("Parsed", parsed)
 
-  const isUnderDevelopment = process.env.NODE_ENV === 'development'
-  // isUnderDevelopment &&
-  // Shows server side log. (Validation on Server as well as the client)
+	const isUnderDevelopment = process.env.NODE_ENV === "development"
+	// isUnderDevelopment &&
+	// Shows server side log. (Validation on Server as well as the client)
 
-  const issues = parsed.error?.issues.map((issue) => issue.message)
+	const issues = parsed.error?.issues.map((issue) => issue.message)
 
-  if (!parsed.success) {
-    const fields: Record<string, string> = {}
-    for (const key of Object.keys(formData)) {
-      fields[key] = formData[key].toString()
-    }
-    return {
-      status: 'error',
-      message: `Invalid form data`,
-      issues,
-      fields,
-    }
-  }
+	if (!parsed.success) {
+		const fields: Record<string, string> = {}
+		for (const key of Object.keys(formData)) {
+			fields[key] = formData[key].toString()
+		}
+		return {
+			status: "error",
+			message: `Invalid form data`,
+			issues,
+			fields,
+		}
+	}
 
-  if (parsed.data.email.includes('a') && isUnderDevelopment) {
-    console.log('is under development')
-    // Weird Server side proxy that must be included, but this test by default is only to show where server side validation is done.
-    return {
-      status: 'error',
-      message: 'Invalid email address',
-      fields: parsed.data,
-    }
-  }
+	if (parsed.data.email.includes("a") && isUnderDevelopment) {
+		console.log("is under development")
+		// Weird Server side proxy that must be included, but this test by default is only to show where server side validation is done.
+		return {
+			status: "error",
+			message: "Invalid email address",
+			fields: parsed.data,
+		}
+	}
 
-  try {
-    const result = await SignUp(parsed.data)
-    if (!result.success) {
-      return { status: 'error', message: result.error?.toString() || 'An error occurred' }
-    }
-    return { status: 'success', message: 'Creating Account' }
-  } catch (error) {
-    return { status: 'error', message: 'An unknown error occurred' }
-  }
+	try {
+		// const result = await SignUp(parsed.data)
+		const result = {
+			success: false,
+			error: "Need to modify 'app/sign-up/actions/signUp.tsx' function according to Payload requirements.",
+		}
+		if (!result.success) {
+			return {
+				status: "error",
+				message: result.error?.toString() || "An error occurred",
+			}
+		}
+		return { status: "success", message: "Creating Account" }
+	} catch (error) {
+		return { status: "error", message: "An unknown error occurred" }
+	}
 }
--- src/app/(app)/sign-up/actions/signUp.ts
+++ src/app/(app)/sign-up/actions/signUp.ts
@@ -1,54 +1,54 @@
-'use server'
+// 'use server'
 
-import { getPayload } from 'payload'
-import config from '@payload-config'
-import { cookies } from 'next/headers'
-import { Customer } from '@/payload-types'
+// import { getPayload } from 'payload'
+// import config from '@payload-config'
+// import { cookies } from 'next/headers'
+// import { Customer } from '@/payload-types'
 
-interface SignUpParams {
-  firstName?: string
-  lastName?: string
-  email: string
-  password: string
-}
+// interface SignUpParams {
+//   firstName?: string
+//   lastName?: string
+//   email: string
+//   password: string
+// }
 
-interface SignUpResponse {
-  success: boolean
-  error?: string
-}
+// interface SignUpResponse {
+//   success: boolean
+//   error?: string
+// }
 
-export type Result = {
-  exp?: number
-  token?: string
-  user?: Customer
-}
+// export type Result = {
+//   exp?: number
+//   token?: string
+//   user?: Customer
+// }
 
-export async function SignUp({
-  email,
-  password,
-  firstName,
-  lastName,
-}: SignUpParams): Promise<SignUpResponse> {
-  const payload = await getPayload({ config })
-  try {
-    const result: Result = await payload.SignUp({
-      collection: 'customers',
-      data: { email, password },
-    })
+// export async function SignUp({
+//   email,
+//   password,
+//   firstName,
+//   lastName,
+// }: SignUpParams): Promise<SignUpResponse> {
+//   const payload = await getPayload({ config })
+//   try {
+//     const result: Result = await payload.SignUp({
+//       collection: 'customers',
+//       data: { email, password },
+//     })
 
-    if (result.token) {
-      const cookieStore = await cookies()
-      cookieStore.set('payload-token', result.token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        path: '/',
-      })
-      return { success: true }
-    } else {
-      return { success: false, error: 'Invalid email or password' }
-    }
-  } catch (error) {
-    console.error('SignUp error', error)
-    return { success: false, error: 'An error occurred' }
-  }
-}
+//     if (result.token) {
+//       const cookieStore = await cookies()
+//       cookieStore.set('payload-token', result.token, {
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === 'production',
+//         path: '/',
+//       })
+//       return { success: true }
+//     } else {
+//       return { success: false, error: 'Invalid email or password' }
+//     }
+//   } catch (error) {
+//     console.error('SignUp error', error)
+//     return { success: false, error: 'An error occurred' }
+//   }
+// }
